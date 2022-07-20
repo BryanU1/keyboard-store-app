5037fa875d090f665218a78f540475a727833bf9
BryanU <ungbryan01@gmail.com>
2022-07-19 18:49:00 -0700

    Add navigation bar

---
+++ client/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav className="nav-bar">
+      <h1>Logo</h1>
+      <ul>
+        <li>About</li>
+        <li>Shop</li>
+        <li>Build</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;

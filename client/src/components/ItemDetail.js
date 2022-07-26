f0fc93b8a0989edbeb0af12537e26fdf5ee6ae5d
BryanU <ungbryan01@gmail.com>
2022-07-26 15:58:21 -0700

    Update item detail page

--- client/src/components/ItemDetail.js
+++ client/src/components/ItemDetail.js
@@ -1,8 +1,10 @@
-import react from 'react';
+import { useParams } from 'react-router-dom';
 
 function ItemDetail() {
+  const params = useParams();
   return (
     <div>
+      ProductID is {params.id}
     </div>
   )
 }

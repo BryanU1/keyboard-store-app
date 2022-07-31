59433dbf4a4cc13bf4b48bdad7c5d4196b772c50
BryanU <ungbryan01@gmail.com>
2022-07-30 20:07:39 -0700

    Implement item_list controller

--- api/controllers/itemController.js
+++ api/controllers/itemController.js
@@ -7,9 +7,16 @@ exports.index = function(req, res) {
   });
 }
 
+// Display list of all items
 exports.item_list = function(req, res) {
-  res.send('NOT IMPLEMENTED: Item List');
-}
+  Item.find({}, 'name price')
+    .sort({name: 1})
+    .exec(function (err, list_items) {
+      if (err) {return next(err); }
+      // Successful, so render
+      res.render('item_list', {title: 'Item List', item_list: list_items});
+    })
+  }
 
 exports.item_detail = function(req, res) {
   res.send('NOT IMPLEMENTED: Item detail:' + req.params.id)
